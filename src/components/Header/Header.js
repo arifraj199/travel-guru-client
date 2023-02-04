60ee47ce748fde9f724a6a39a942ac3a0fe2e96c
arifraj199 <karif9514@gmail.com>
2023-02-04 12:35:52 +0600

    navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,56 @@
 import React from "react";
-import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Form, Image, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="light" variant="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Form className="d-flex">
+        <Link to="/">
+          <Image style={{ height: "60px" }} src={logo}></Image>
+        </Link>
+        <Form className="d-flex ">
           <Form.Control
             type="search"
-            placeholder="Search Your Destination..."
-            className="me-2"
+            placeholder="Search Destination..."
+            className="ms-5"
             aria-label="Search"
           />
         </Form>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto d-flex align-items-center">
+            <Link
+              className="text-decoration-none ms-4 text-black fw-semibold"
+              to="/news"
+            >
+              News
+            </Link>
+            <Link
+              className="text-decoration-none ms-4 text-black fw-semibold"
+              to="/destination"
+            >
+              Destination
+            </Link>
+            <Link
+              className="text-decoration-none ms-4 text-black fw-semibold"
+              to="/blog"
+            >
+              Blog
+            </Link>
+            <Link
+              className="text-decoration-none ms-4 text-black fw-semibold"
+              to="/contact"
+            >
+              Contact
+            </Link>
+            <Link
+              className="text-decoration-none ms-4 text-black fw-semibold"
+              to="/login"
+            >
+              <Button variant="warning">Login</Button>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
