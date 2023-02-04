3d7a420b190f6803d4d9567dfe2ff7c1864663dd
arifraj199 <karif9514@gmail.com>
2023-02-04 12:18:07 +0600

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search Your Destination..."
+            className="me-2"
+            aria-label="Search"
+          />
+        </Form>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZfSh4j0IUtHChH-hEPo83l6GuGgj36Kg",
+  authDomain: "travel-guru-7a7da.firebaseapp.com",
+  projectId: "travel-guru-7a7da",
+  storageBucket: "travel-guru-7a7da.appspot.com",
+  messagingSenderId: "82387661011",
+  appId: "1:82387661011:web:c0febe01ab8237319f6426"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
